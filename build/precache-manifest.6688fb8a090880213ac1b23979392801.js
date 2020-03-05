df7f3d86ce488b88b5a0e0559f610ee0353f29d4
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-03-05 16:51:24 +0200

    16:51

---
+++ build/precache-manifest.6688fb8a090880213ac1b23979392801.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57afefed420e14a63d1be9ca550c384",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "c58fbfb6f157e647fb8d",
+    "url": "/React/static/css/2.67514248.chunk.css"
+  },
+  {
+    "revision": "532f2d53e1af0655f227",
+    "url": "/React/static/css/main.d1ce3b25.chunk.css"
+  },
+  {
+    "revision": "c58fbfb6f157e647fb8d",
+    "url": "/React/static/js/2.3c3bfad9.chunk.js"
+  },
+  {
+    "revision": "f69d32277f0cf814090a69f646599e5e",
+    "url": "/React/static/js/2.3c3bfad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "532f2d53e1af0655f227",
+    "url": "/React/static/js/main.00da8491.chunk.js"
+  },
+  {
+    "revision": "4a4beeb7c7905fd5775b",
+    "url": "/React/static/js/runtime-main.257c1ead.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/React/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/React/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/React/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/React/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
