73d5da409679d8359e33be68ef3438d6e7162ef9
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-03-05 16:52:56 +0200

    Updates

---
+++ precache-manifest.5aa6d58ee0d936e71edac85c546d1eca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5706543b370e28f0d73c28affb4de2c",
+    "url": "/React-HW2-2-phonebook/index.html"
+  },
+  {
+    "revision": "838b306a2f562e1c660a",
+    "url": "/React-HW2-2-phonebook/static/css/2.9c3346e0.chunk.css"
+  },
+  {
+    "revision": "404b55990a7842ee26e3",
+    "url": "/React-HW2-2-phonebook/static/css/main.09c28c0c.chunk.css"
+  },
+  {
+    "revision": "838b306a2f562e1c660a",
+    "url": "/React-HW2-2-phonebook/static/js/2.064da478.chunk.js"
+  },
+  {
+    "revision": "f69d32277f0cf814090a69f646599e5e",
+    "url": "/React-HW2-2-phonebook/static/js/2.064da478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404b55990a7842ee26e3",
+    "url": "/React-HW2-2-phonebook/static/js/main.3fa3ea32.chunk.js"
+  },
+  {
+    "revision": "0fc6204d07819fded2da",
+    "url": "/React-HW2-2-phonebook/static/js/runtime-main.f8e309c2.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/React-HW2-2-phonebook/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/React-HW2-2-phonebook/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/React-HW2-2-phonebook/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/React-HW2-2-phonebook/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
